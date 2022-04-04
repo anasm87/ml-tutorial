1705a7cc2c7ee7508ce5078c8a2202d4b1d73a19
anas mhana <anasm87@gmail.com>
2022-04-04 18:17:46 +0200

    remove commitsfdfsd jnjllssjlkjdlöösl

--- src/predict.py
+++ src/predict.py
@@ -5,7 +5,7 @@ import pickle
 file_to_open = open("data/models/baummethoden_lr.pickle", 'rb')
 trained_model = pickle.load(file_to_open)
 file_to_open.close()
-
+i =0
 # load data that we want predictions for
 prediction_data = pd.read_csv('data/prediction-data.csv', sep=";")
 print(trained_model.predict(prediction_data))
